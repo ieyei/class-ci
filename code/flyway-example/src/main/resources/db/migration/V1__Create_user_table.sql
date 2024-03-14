3ec656b43a80b0038851746883e0c3e6275c0028
임형수 <pj.neal.im@kalmate.net>
2024-03-15 06:38:26 +0900

    docs: Update Flyway.md with information on Flyway CLI, Gradle, and CI integration.

---
+++ code/flyway-example/src/main/resources/db/migration/V1__Create_user_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS users
+(
+    id         INT  PRIMARY KEY,
+    username   VARCHAR(50)  NOT NULL,
+    email      VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
---
+++ code/flyway-example/src/main/resources/db/migration/V2__Add_admin_user.sql
@@ -0,0 +1,1 @@
+INSERT INTO users (id, username, email) VALUES (1, 'admin', 'admin@example.com');
